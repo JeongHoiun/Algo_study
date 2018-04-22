e2fdf2fc60c37c98136b895b1bc1e10f93036df9
JeongHoiun <un4255@gmail.com>
2018-04-22 22:44:03 +0900

    modify 11727

--- 180424_acmicpc/11727.java
+++ 180424_acmicpc/11727.java
@@ -12,7 +12,37 @@ public class Main {
 	}
 	
 	int nTile(int n) {
-		//  n(N_3) = n(N_2)+n(N_1) 
+		// acmicpc.net/problem/11727
+import java.util.Scanner;
+
+public class Main {
+	int dp[];
+	void Tiling() {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		dp = new int[n+1];
+		dp[1] = 1;
+		System.out.println(nTile(n)%10007);
+	}
+	
+	int nTile(int n) {
+		//  X(n) = 2X(n-1)+1 (n is even number)
+		//  X(n) = 2X(n-1)-1 (n is odd number)
+		if(dp[n] > 0) 
+			return dp[n];
+		else if(n%2==0)
+			dp[n] = nTile(n-1)*2+1;
+		else if(n%2==1)
+			dp[n] = nTile(n-1)*2-1;
+		return dp[n]%10007;
+	}
+	
+	public static void main(String[] args) {
+		new Main().Tiling();
+	}
+
+}
+
 		if(dp[n] > 0) 
 			return dp[n];
 		else if(n%2==0)
