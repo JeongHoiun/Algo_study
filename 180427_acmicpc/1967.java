9c04a7aa7eeb8af2dbb14b7d4ea53938f09a6dfb
JeongHoiun <un4255@gmail.com>
2018-04-27 07:58:06 +0900

    BOJ 1967 [Diameter of Tree]

---
+++ 180427_acmicpc/1967.java
@@ -0,0 +1,70 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+// https://www.acmicpc.net/problem/1167
+// out of memory Error ... 100000*100000
+class Main{
+	static int v, u, r;
+	static ArrayList<Node>[] adj;
+	static int[] dist;
+
+	private void solve() {
+		Scanner sc =new Scanner(System.in);
+		adj = (ArrayList<Node>[]) new ArrayList[10001];
+		dist = new int[10001];
+
+		for (int i = 1; i <= 10000; i++) {
+			adj[i] = new ArrayList<>();
+		}
+
+		int n = sc.nextInt();
+
+		for (int i = 0; i < n - 1; i++) {
+			int parent = sc.nextInt();
+			int child = sc.nextInt();
+			int weight = sc.nextInt();
+
+			adj[parent].add(new Node(child, weight));
+			adj[child].add(new Node(parent, weight));
+		}
+
+		dfs(1, 0);
+
+		r = 0;
+		dist = new int[10001];
+
+		dfs(u, 0);
+		System.out.println(r);
+	}
+
+	public static void dfs(int v, int d) {
+		dist[v] = d;
+
+		if (dist[v] > r) {
+			r = dist[v];
+			u = v;
+		}
+
+		for (Node node : adj[v]) {
+			int next = node.v;
+			int weight = node.w;
+
+			if (dist[next] == 0) {
+				dfs(next, d + weight);
+			}
+		}
+
+	}
+	public static void main(String[] args) {
+		new Main().solve();
+	}
+}
+
+class Node {
+	int v;
+	int w;
+
+	Node(int v, int w) {
+		this.v = v;
+		this.w = w;
+	}
+}
