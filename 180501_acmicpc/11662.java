26cd67abc3c8dc8506232e1a4568db0b0088da64
JeongHoiun <un4255@gmail.com>
2018-05-01 13:06:16 +0900

    BOJ 11662 [Minho ang Kangho](Incomplete)

---
+++ 180501_acmicpc/11662.java
@@ -0,0 +1,57 @@
+import java.util.Scanner;
+
+public class Main {
+	void MinhoNKangho() {
+		Scanner sc= new Scanner(System.in);
+		double ax,ay,bx,by,cx,cy,dx,dy;
+		ax = sc.nextDouble();
+		ay = sc.nextDouble();
+		bx = sc.nextDouble();
+		by = sc.nextDouble();
+		cx = sc.nextDouble();
+		cy = sc.nextDouble();
+		dx = sc.nextDouble();
+		dy = sc.nextDouble();
+		
+		
+		double min = Math.abs(Math.sqrt((cx-ax)*(cx-ax)+(cy-ay)*(cy-ay)));
+		double mino_leftx = ax;
+		double mino_lefty = ay;
+		double mino_rightx = bx;
+		double mino_righty = by;
+		double kang_leftx = cx;
+		double kang_lefty = cy;
+		double kang_rightx = dx;
+		double kang_righty = dy;
+		while(Math.abs(mino_righty-mino_lefty)>0.0000001) {
+			double mino_midx = (mino_leftx+mino_rightx)/2.0f;
+			double mino_midy = (mino_lefty+mino_righty)/2.0f;
+			double kang_midx = (kang_leftx+kang_rightx)/2.0f;
+			double kang_midy = (kang_lefty+kang_righty)/2.0f;
+			
+			double mid_result = Math.abs(Math.sqrt((kang_midx-mino_midx)*(kang_midx-mino_midx)
+					+((kang_midy-mino_midy)*((kang_midy-mino_midy)))));
+			
+			
+			if(min>mid_result) {
+				 mino_leftx = mino_midx;
+				 mino_lefty = mino_midy;
+				 kang_leftx = kang_midx;
+				 kang_lefty = kang_midy;
+				 min = mid_result;
+			}else {
+				mino_rightx = mino_midx;
+				mino_righty = mino_midy;
+				kang_rightx = kang_midx;
+				kang_righty = kang_midy;
+			}
+				
+		}
+		System.out.println(Double.parseDouble(String.format("%.10f",min)));
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		new Main().MinhoNKangho();
+	}
+
+}
